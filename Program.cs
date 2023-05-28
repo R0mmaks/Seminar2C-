e11f3d0490501e64d84d339a64b613bb9333c2a8
Rommaks <romanmaksin22@gmail.com>
2023-05-28 20:17:36 +0300

    записал решение задачи15

--- Program.cs
+++ Program.cs
@@ -5,23 +5,20 @@
 // 782 -> 8
 // 918 -> 1
 
- Console.WriteLine ("Введите трёхзначное число");
+//  Console.WriteLine ("Введите трёхзначное число");
 
-int num1=Convert.ToInt32(Console.ReadLine ());
+// int num1=Convert.ToInt32(Console.ReadLine ());
 
-
-if ((num1>100) && (num1<1000))
-{
-    int num2=num1/10;
-    int num3=num2%10;
-    
-    Console.WriteLine (num3);
-}
-
-else
-{
-    Console.WriteLine ("Вы ввели не трёхзначное число");
-}
+// if ((num1>100) && (num1<1000))
+// {
+//     int num2=num1/10;
+//     int num3=num2%10;   
+//     Console.WriteLine (num3);
+// }
+// else
+// {
+//     Console.WriteLine ("Вы ввели не трёхзначное число");
+// }
 // _______________________________________________________
 // 
 // Задача 13: Напишите программу, 
@@ -30,6 +27,24 @@ else
 // 645 -> 5
 // 78 -> третьей цифры нет
 // 32679 -> 6
+//  Console.WriteLine ("Введите число");
+
+// int num1=Convert.ToInt32(Console.ReadLine ());
+
+// while (num1>999)
+// {
+//     int num2=num1/10;
+//     int num3=num2%10;   
+// }
+// if (num1<100)
+// {
+//      Console.WriteLine ("Третьей цифры нет");
+//    }
+// else
+// {
+//     Console.WriteLine (num3);
+// }
+// _______________________________________________________________
 
 // Задача 15: Напишите программу, которая принимает 
 // на вход цифру, обозначающую день недели, и проверяет, 
@@ -37,3 +52,23 @@ else
 // 6 -> да
 // 7 -> да
 // 1 -> нет
+Console.WriteLine ("Введите число");
+
+int num=Convert.ToInt32(Console.ReadLine ());
+{
+    if (num>0 && num<8)
+    {
+        if (num == 7 || num == 6)
+        {
+            Console.Write("Это выходной день");
+        }
+    else
+        {
+        Console.Write("Это рабочий день");
+        }
+    }
+    else 
+        {
+        Console.Write("Пожалуйста,введите число из диапазона от 1 до 7");
+        }
+}
